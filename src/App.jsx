e32f2a435f61c0e357288e902361e260514a06a6
ABHIRUP <abhirupsaha02@gmail.com>
2023-09-12 02:44:59 +0530

    some style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,135 @@
-import React, { useState, useRef } from 'react';
-import './app.css';
-import logo from './assets/font.png';
-import sound from './assets/Avengers.mp3';
+import React, { useState, useRef } from 'react'
+import './app.css'
+import logo from './assets/font.webp'
+import sound from './assets/Avengers.mp3'
 
 export default function App() {
-  const audioRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    contact: '',
+    college: ''
+  })
 
-  function togglePlay() {
-    const audio = audioRef.current;
+  const togglePlay = () => {
+    const audio = audioRef.current
 
     if (isPlaying) {
-      audio.pause();
+      audio.pause()
     } else {
       audio.volume = 0.2
       audio.play()
     }
 
-    setIsPlaying(!isPlaying);
+    setIsPlaying(!isPlaying)
   }
 
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    contact: "",
-    college:""
-  });
-
   const getUserData = (event) => {
-    const { name, value } = event.target;
-    setUser({ ...user, [name]: value });
-  };
+    const { name, value } = event.target
+    setUser({ ...user, [name]: value })
+  }
 
   const postData = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const { name, email, contact, college } = user;
+    const { name, email, contact, college } = user
 
     try {
-      const databaseURL =
-        "https://codefusionpaymentgateway-default-rtdb.firebaseio.com"; 
-      const path = "/users.json"; 
+      const databaseURL = process.env.REACT_APP_DATABASE_URL
+      const path = 'users.json'
 
-      const response = await fetch(`${databaseURL}${path}`, {
-        method: "POST",
+      const response = await fetch(`${databaseURL}/${path}`, {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json"
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          name,
-          email,
-          contact,
-          college
+          name: name.trim(),
+          email: email,
+          contact: contact,
+          college: college.trim()
         })
-      });
+      })
 
       if (response.ok) {
-        console.log("Data successfully posted.");
-      } else {
-        console.error("Failed to post data.");
+        console.log('Data successfully posted.')
       }
-    } catch (error) {
-      console.error("Error:", error);
     }
-  };
+    catch (error) {
+      console.error(error)
+    }
+  }
 
 
   return (
-    <div className="Background">
-      <form>
-        <img src={logo} alt="Logo" width="200px" />
-        <div className="input">
-          <label htmlFor="fname">Name:</label>
-          <input type="text" id="fname" value={user.name}
-            onChange={getUserData}
-            name="name" required />
-        </div>
-        <div className="input">
-          <label htmlFor="femail">Email:</label>
-          <input type="text" id="femail" value={user.email}
-            onChange={getUserData}
-            name="email" required />
-        </div>
-        <div className="input">
-          <label htmlFor="fcontact">Contact:</label>
-          <input type="text" id="fcontact" value={user.contact}
-            onChange={getUserData}
-            name="contact"  required />
-        </div>
-        <div className="input">
-          <label htmlFor="fcontact">College:</label>
-          <input type="text" id="fcontact" name="college" value={user.college} onChange={getUserData} />
-        </div>
-        <button type="button" className="btn" onClick={postData}>
-          Make Payment
-        </button>
-        <button type="button"  onClick={togglePlay}>
-          {isPlaying ? 'Pause Sound' : 'Play Sound'}
-        </button>
-      </form>
-      <audio ref={audioRef} src={sound} />
+    <div className='Background'>
+      <div className='form-container'>
+        <form>
+          <div className='audio' onClick={togglePlay}>
+            <img src={logo} alt='Logo' loading='lazy' />
+            <div>
+              {
+                isPlaying
+                  ? <ion-icon name="pause-circle-outline"></ion-icon>
+                  : <ion-icon name="play-outline"></ion-icon>
+              }
+            </div>
+          </div>
+          <div className='input'>
+            <label htmlFor='fname'>Name:</label>
+            <input
+              id='fname'
+              type='text'
+              value={user.name}
+              onChange={getUserData}
+              name='name'
+              required
+              spellCheck='false'
+            />
+          </div>
+          <div className='input'>
+            <label htmlFor='femail'>Email:</label>
+            <input
+              id='femail'
+              type='text'
+              value={user.email.trim()}
+              onChange={getUserData}
+              name='email'
+              required
+              spellCheck='false'
+            />
+          </div>
+          <div className='input'>
+            <label htmlFor='fcontact'>Contact:</label>
+            <input
+              id='fcontact'
+              type='text'
+              value={user.contact.trim()}
+              onChange={getUserData}
+              name='contact'
+              required
+              spellCheck='false'
+            />
+          </div>
+          <div className='input'>
+            <label htmlFor='fcontact'>College:</label>
+            <input
+              id='fcontact'
+              type='text'
+              name='college'
+              value={user.college}
+              onChange={getUserData}
+              spellCheck='false'
+            />
+          </div>
+          <button type='submit' className='btn' onClick={postData}>
+            Make Payment
+          </button>
+        </form>
+        <audio ref={audioRef} src={sound} />
+      </div>
     </div>
-  );
+  )
 }
