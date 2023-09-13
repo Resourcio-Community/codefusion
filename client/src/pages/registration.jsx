0fbf6deda59ae0b9fd707af6b71e1ca71bbc5118
Anushka Bhowmick <76967222+Anushka-Bhowmick@users.noreply.github.com>
2023-09-13 18:58:13 +0530

    Responsive update

--- client/src/pages/registration.jsx
+++ client/src/pages/registration.jsx
@@ -1,108 +1,99 @@
-import { useContext, useState } from "react"
-import logo from '../assets/font.webp'
-import { Context } from "../context/Context"
-import { useHistory } from "react-router-dom"
-import { toast, ToastContainer } from "react-toastify"
-import "react-toastify/dist/ReactToastify.css"
+import { useContext, useState } from "react";
+import logo from "../assets/font.webp";
+import { Context } from "../context/Context";
+import { useHistory } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Registration = () => {
-  const { user, setUser, setAuth } = useContext(Context)
-  const history = useHistory()
+  const { user, setUser, setAuth } = useContext(Context);
+  const history = useHistory();
 
   const getUserData = (event) => {
-    const { name, value } = event.target
-    setUser({ ...user, [name]: value })
-  }
+    const { name, value } = event.target;
+    setUser({ ...user, [name]: value });
+  };
 
   const gotoPayment = () => {
-    let hasError = false
-    if (user.name === '') {
-      hasError = true
+    let hasError = false;
+    const email = user.email.trim();
+    const contact = user.contact.trim();
+    if (user.name === "" || email === "" || !email.includes("@") ||contact === "" || !/^\d{10}$/.test(user.contact)) {
+      hasError = true;
     }
-
-    if (user.email === '' && !user.email.includes('@')) {
-      hasError = true
-    }
-
-    if (user.contact === '' && !/^\d{10}$/.test(user.contact)) {
-      hasError = true
-    }
-
-
+    
     if (!hasError) {
-      setAuth(true)
-      history.push('/payment')
+      setAuth(true);
+      history.push("/payment");
     } else {
-      toast.warn("Fill up required fields")
+      toast.warn("Fill up required fields");
     }
-  }
+  };
 
   return (
-    <div className='Background'>
-      <div className='form-container'>
-        <img src={logo} alt='Logo' loading='lazy' />
+    <div className="Background">
+      <div className="form-container">
+        <img src={logo} alt="Logo" loading="lazy" />
         <div className="form">
-          <div className='input'>
-            <label htmlFor='fname'>Name*</label>
+          <div className="input">
+            <label htmlFor="fname">Name*</label>
             <input
-              id='fname'
-              type='text'
+              id="fname"
+              type="text"
               value={user.name}
               onChange={getUserData}
-              name='name'
+              name="name"
               required
-              spellCheck='false'
+              spellCheck="false"
               placeholder="John Doe"
             />
           </div>
-          <div className='input'>
-            <label htmlFor='femail'>Email*</label>
+          <div className="input">
+            <label htmlFor="femail">Email*</label>
             <input
-              id='femail'
-              type='text'
+              id="femail"
+              type="text"
               value={user.email.trim()}
               onChange={getUserData}
-              name='email'
+              name="email"
               required
-              spellCheck='false'
+              spellCheck="false"
               placeholder="example@gmail.com"
             />
           </div>
-          <div className='input'>
-            <label htmlFor='fcontact'>Contact*</label>
+          <div className="input">
+            <label htmlFor="fcontact">Contact*</label>
             <input
-              id='fcontact'
-              type='text'
+              id="fcontact"
+              type="text"
               value={user.contact.trim()}
               onChange={getUserData}
-              name='contact'
+              name="contact"
               required
-              spellCheck='false'
+              spellCheck="false"
               placeholder="Phone no."
             />
           </div>
-          <div className='input'>
-            <label htmlFor='fcontact'>College</label>
+          <div className="input">
+            <label htmlFor="fcontact">College</label>
             <input
-              id='fcontact'
-              type='text'
-              name='college'
+              id="fcontact"
+              type="text"
+              name="college"
               value={user.college}
               onChange={getUserData}
-              spellCheck='false'
+              spellCheck="false"
               placeholder="College Name"
             />
           </div>
-          <button type='button' className='btn' onClick={gotoPayment}>
+          <button type="button" className="btn" onClick={gotoPayment}>
             Submit
           </button>
         </div>
       </div>
-      <ToastContainer
-        position="bottom-left"
-      />
+      <ToastContainer position="top-right" theme="dark" />
     </div>
-  )
-}
+  );
+};
 
-export default Registration
+export default Registration;
